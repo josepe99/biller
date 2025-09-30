f060a118073d0aac34e368e0639596c8dbd0b64d
Josepe <josepedrozo99@gmail.com>
2025-09-30 14:34:45 -0300

    refactor(PDFDownload): remove unused API route for PDF download

--- pages/api/pdfdownload.ts
+++
@@ -1,6 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import pdfDownloadController from "@/lib/controllers/pdfdownload.controller";
-
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  return pdfDownloadController(req, res);
-}
